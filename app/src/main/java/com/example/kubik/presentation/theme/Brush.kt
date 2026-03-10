2dd1965460dc657707d0a489c4b0160d7401cbb6
Tsoy <maratts12342334@gmail.com>
2026-03-10 23:20:34 +0300

    Project has been renamed (КУБИК)

---
+++ app/src/main/java/com/example/kubik/presentation/theme/Brush.kt
@@ -0,0 +1,15 @@
+package com.example.kubik.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+
+object AppBrushes{
+    val titleGradient: Brush
+    @Composable
+    get() = Brush.linearGradient(
+        colors = listOf(
+            titleGradient1,
+            titleGradient2
+        )
+    )
+}
